39b9d13b77dc8b6c1fa06d4e13b91c999e8c32d9
Guilherme Gazzo <guilhermegazzo@gmail.com>
2022-03-25 11:36:42 -0300

    README

--- packages/client/src/lib/ILogger.d.ts
+++
@@ -1,7 +0,0 @@
-/** Loggers need to provide the same set of methods */
-export interface ILogger {
-    debug: (...args: unknown[]) => void;
-    info: (...args: unknown[]) => void;
-    warn: (...args: unknown[]) => void;
-    error: (...args: unknown[]) => void;
-}
--- packages/client/src/lib/convertHostToWs.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function convertHostToWs<T extends string>(host: Host<T>): WsHost<T>;
-export declare type Host<T extends string = string> = `http://${T}` | `https://${T}` | WsHost<T>;
-export declare type WsHost<T extends string> = `ws://${T}` | `wss://${T}`;
